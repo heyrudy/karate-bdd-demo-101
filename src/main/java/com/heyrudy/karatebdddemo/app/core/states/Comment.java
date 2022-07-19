8458a4ade1168ac784963535281725ed4477354f
Turing00 <kokou.koumagnagottoh@gmail.com>
2022-07-20 00:49:07 +0200

    :recycle: core comment entity

--- src/main/java/com/heyrudy/karatebdddemo/app/core/states/Comment.java
+++ src/main/java/com/heyrudy/karatebdddemo/app/core/states/Comment.java
@@ -1,6 +1,10 @@
 package com.heyrudy.karatebdddemo.app.core.states;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @NoArgsConstructor
